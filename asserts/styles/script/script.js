e096745557bb0dc8ef0e583199d56aba24b8cea5
kadjialan <alankadji7@gmail.com>
2022-12-23 15:12:16 +0100

    js modification

--- asserts/styles/script/script.js
+++ asserts/styles/script/script.js
@@ -6,22 +6,21 @@ const list = document.querySelector('.characters')
 
 balanceButton.onclick = function balancing () {
   const input1 = mass1.value
+  console.log(input1)
   const input2 = mass2.value
   const answers = list.value
   const result = answers.split(',')
-  
-      if (input1 === input2) {
-        holder.innerHTML = 'balance'
-      } 
-      
-      else(input1 != input2) 
-        for (let i = 0; i < result.length; i++) {
-          let ans =  parseInt(input1) + parseInt(result[i])
-          let ans2 = parseInt(input2) + parseInt(result[i])
-          
-        }
-      
-    
-  
-  
+
+  if (input1 === input2) {
+    holder.innerHTML = 'balance'
+  } else (input1 !== input2)
+  for (let i = 0; i < result.length; i++) {
+    for (let j = 0; j < result.length; j++) {
+      let valid = parseInt(input1) + parseInt(result[i]) === parseInt(input2) + parseInt(result[j])
+      if (valid) {
+        holder.innerHTML = `add ${result[i]}, ${result[j]}`
+      }
+    }
+  }
 }
+
