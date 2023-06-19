5d1e1ae31faeb09eb6abb5f5936411d15a455a6f
kadjialan <alankadji7@gmail.com>
2023-06-20 00:58:46 +0100

    logique and ui modification

--- asserts/styles/script/script.js
+++ asserts/styles/script/script.js
@@ -1,26 +1,30 @@
-const mass1 = document.querySelector('.weight1')
-const mass2 = document.querySelector('.weight2')
-const holder = document.querySelector('.display')
-const balanceButton = document.querySelector('.start')
-const list = document.querySelector('.characters')
+const mass1 = document.querySelector(".weight1");
+const mass2 = document.querySelector(".weight2");
+const holder = document.querySelector(".display");
+const balanceButton = document.querySelector(".start");
+const list = document.querySelector(".characters");
 
-balanceButton.onclick = function balancing () {
-  const input1 = mass1.value
-  const input2 = mass2.value
-  const answers = list.value
-  const result = answers.split(',')
+balanceButton.onclick = function balancing() {
+  const input1 = mass1.value;
+  const input2 = mass2.value;
+  const answers = list.value;
+  const result = answers.split(",");
 
   for (let i = 0; i < result.length; i++) {
     for (let j = 0; j < result.length; j++) {
-      console.log(result[j])
+      let firstSum = parseInt(input1) + parseInt(result[i]);
+      let secondSum = parseInt(input2) + parseInt(result[j]);
+      console.log(firstSum, secondSum);
       if (input1 === input2) {
-        holder.innerHTML = 'balance'
-      } else if (parseInt(input1) + parseInt(result[i]) === parseInt(input2) + parseInt(result[j])) {
-        holder.innerHTML = `add ${result[i]}, ${result[j]}`
-        break
+        holder.innerHTML = "balance";
       } else {
-        holder.innerHTML = 'not balance'
+        if (firstSum === secondSum) {
+          holder.innerHTML = `add ${result[i]}, ${result[j]}`;
+          break;
+        } else {
+          holder.innerHTML = "not balance";
+        }
       }
     }
   }
-}
+};
